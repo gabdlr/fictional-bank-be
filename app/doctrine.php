6e2280daacd1306a08a2c3582d05e9b4fd45b742
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-07-26 22:29:15 -0300

    fix(doctrine): add missing class import

--- app/doctrine.php
+++ app/doctrine.php
@@ -1,5 +1,4 @@
 <?php
-use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 use Doctrine\Common\Cache\Psr6\DoctrineProvider;
@@ -7,10 +6,11 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use App\Application\Settings\SettingsInterface;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        EntityManager::class => function (ContainerInterface $c) {
+      EntityManager::class => function (ContainerInterface $c) {
             $settings = ($c->get(SettingsInterface::class))->get('doctrine');
             // Use the ArrayAdapter or the FilesystemAdapter depending on the value of the 'dev_mode' setting
             // You can substitute the FilesystemAdapter for any other cache you prefer from the symfony/cache library
