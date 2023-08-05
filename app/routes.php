b0471879a5b2f4df78b69c0a07f458085e1a7525
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-08-05 18:54:13 -0300

    refactor: removes unnecesary files and endpoints

--- app/routes.php
+++ app/routes.php
@@ -7,23 +7,18 @@ use App\Application\Actions\ProductoFinanciero\DeleteProductoFinancieroAction;
 use App\Application\Actions\ProductoFinanciero\ListProductosFinancierosAction;
 use App\Application\Actions\ProductoFinanciero\UpdateProductoFinancieroAction;
 use App\Application\Actions\ProductoFinanciero\VerifyProductoFinancieroAction;
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
 use App\Application\Middleware\JsonBodyParserMiddleware;
 use App\Application\Middleware\OfficerAccountIdentityMiddleware;
 use Psr\Http\Message\ServerRequestInterface;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
-        return $response;
-    });
-
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
         return $response;
     });
 
@@ -34,11 +29,6 @@ return function (App $app) {
         $group->delete('', DeleteProductoFinancieroAction::class);
         $group->get('verification', VerifyProductoFinancieroAction::class);
     })->addMiddleware(new OfficerAccountIdentityMiddleware($app));
-
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
 
     $customErrorHandler = function (
         ServerRequestInterface $request,
