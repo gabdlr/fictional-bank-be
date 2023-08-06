3af0ae7f49e6e6dde4005350c263c9f309c47a56
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-08-05 22:19:52 -0300

    fix: modifications for railway

--- public/index.php
+++ public/index.php
@@ -11,8 +11,6 @@ use Slim\Factory\ServerRequestCreatorFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../.env');
-$dotenv->safeLoad();
 $container = require __DIR__ . '/../app/DI-container.php';
 
 // Instantiate the app
